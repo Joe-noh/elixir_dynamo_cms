6f1e8d65298c5a4fd8fbe0baa61722316102cf27
Joe Honzawa <jhonzawa@stn.nagaokaut.ac.jp>
2014-07-03 13:41:28 +0900

    separate templates into layout and content

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -2,10 +2,8 @@ defmodule ApplicationRouter do
   use Dynamo.Router
 
   prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params])
+    conn = conn.fetch([:cookies, :params])
+    conn.assign(:layout, "main")
   end
 
   # It is common to break your Dynamo into many
@@ -13,7 +11,6 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
     render conn, "index.html"
   end
 end
