12a9e3e8a742d8eabbe7087b5a46df93f5705aef
Joe Honzawa <jhonzawa@stn.nagaokaut.ac.jp>
2014-07-03 19:46:36 +0900

    render markdown at /articles/:year/:month/:day/:title

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -13,4 +13,27 @@ defmodule ApplicationRouter do
   get "/" do
     render conn, "index.html"
   end
+
+  get "/articles/:year/:month/:day/:title" do
+    year  = Binary.Dict.get(conn.params, :year)
+    month = Binary.Dict.get(conn.params, :month)
+    day   = Binary.Dict.get(conn.params, :day)
+    title = Binary.Dict.get(conn.params, :title)
+
+    render_markdown(conn, year, month, day, title)
+  end
+
+  defp render_markdown(conn, year, month, day, title) do
+    decoded_title = URI.decode(title)
+    filepath = URI.decode(Path.join(["articles", year, month, day, decoded_title]) <> ".md")
+    case File.read(filepath) do
+      {:ok, content} ->
+        conn = conn.assign(:title, decoded_title)
+        conn = conn.assign(:body, Markdown.to_html(content, tables: true))
+        render conn, "article.html"
+      {:error, _} ->
+        conn = conn.assign(:code, 404)
+        render conn, "error.html"
+    end
+  end
 end
